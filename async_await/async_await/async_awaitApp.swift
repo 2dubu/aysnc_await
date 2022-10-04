c3099ed1f3e4b227d8ece1c3cbe57a4399298ed5
2dubu <2dubu.dev@gmail.com>
2022-10-05 00:03:14 +0900

    create xcode project file

---
+++ async_await/async_await/async_awaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_awaitApp.swift
+//  async_await
+//
+//  Created by 이건우 on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct async_awaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
