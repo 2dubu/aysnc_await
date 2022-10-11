205b3da924759361b17116d110398322e741a817
2dubu <2dubu.dev@gmail.com>
2022-10-11 23:53:00 +0900

    Create DownloadImageAsync file

---
+++ async_await/async_await/DownloadImageAsync.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadImageAsync.swift
+//  async_await
+//
+//  Created by 이건우 on 2022/10/11.
+//
+// Async Await을 활용해 이미지를 다운로드 해보자.
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
