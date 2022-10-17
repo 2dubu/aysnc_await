9883480a91d0c659914d333eccf6006c4f864681
2dubu <2dubu.dev@gmail.com>
2022-10-18 00:22:43 +0900

    download image with async & await

--- async_await/async_await/DownloadImageAsync.swift
+++ async_await/async_await/DownloadImageAsync.swift
@@ -38,6 +38,15 @@ class DownloadImageAsyncLoader {
             .mapError({ $0 })
             .eraseToAnyPublisher()
     }
+    
+    func downloadWithAsync() async throws -> UIImage? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            return handleResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
 }
 
 class DownloadImageAsyncViewModel: ObservableObject {
@@ -45,9 +54,9 @@ class DownloadImageAsyncViewModel: ObservableObject {
     let loader = DownloadImageAsyncLoader()
     var cancellables = Set<AnyCancellable>()
     
-    func fetchImage() {
+    func fetchImage() async {
         
-        /* @escaping code...
+        /* code for download image with @escaping...
         loader.downloadWithEscaping { [weak self] image, error in
             if let image = image {
                 DispatchQueue.main.async {
@@ -57,6 +66,7 @@ class DownloadImageAsyncViewModel: ObservableObject {
         }
          */
         
+        /* code for download image with combine...
         loader.downloadWithCombine()
             .receive(on: DispatchQueue.main)
             .sink { _ in
@@ -65,6 +75,13 @@ class DownloadImageAsyncViewModel: ObservableObject {
                 withAnimation { self?.image = image }
             }
             .store(in: &cancellables)
+         */
+        
+        // DispatchQueue.main 과 동일하게 다음 run loop 를 기다리지 않고 비동기적으로 처리한다.
+        let image = try? await loader.downloadWithAsync()
+        await MainActor.run {
+            withAnimation { self.image = image }
+        }
     }
 }
 
@@ -87,7 +104,13 @@ struct DownloadImageAsync: View {
                 .zIndex(1)
         }
         .onAppear {
-            viewModel.fetchImage()
+            
+            // Task는 Swift가 코드를 병렬로 실행하는 기본 메커니즘이다.
+            // 각 Task는 다른 Task 와 함께 동시(concurrent)에 실행할 수 있는 새로운 비동기 컨텍스트를 제공한다.
+            // 즉, Task{}를 통해 Task(Unstructured Task)를 생성한다. 이 외에도 여러 종류의 Task가 있음.
+            Task {
+                await viewModel.fetchImage()
+            }
         }
     }
 }
